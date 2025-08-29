8ea6cd66b0f56b4f78d74a57e9642117776cec5f
WuJunkai2004 <wujunkai20041123@outlook.com>
2025-08-29 20:34:34 +0800

    init: 初始化前端项目
    添加了主题

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { createApp, ref } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
+
+const count = ref(0)
+const app = createApp({
+  setup() {
+    return {
+      count
+    }
+  }
+})
+app.component('Button', Button)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.mount("#app")
+</script>
+
+<template>
+<div id="app">
+  <Button @click="count++">
+    Count is: {{ count }}
+  </Button>
+</div>
+</template>
+
+<style scoped></style>
