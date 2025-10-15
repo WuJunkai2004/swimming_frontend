b8e8f274f37493a3b4fbbe2ae2744f7517ff24ff
WuJunkai2004 <wujunkai20041123@outlook.com>
2025-10-15 23:03:03 +0800

    fix: 更新菜单栏中的logo图片，使用.webp格式替代.jpg

--- src/components/ComputerMenuBar.vue
+++ src/components/ComputerMenuBar.vue
@@ -7,7 +7,7 @@ const { menuItems } = useMenu();
   <Menubar :model="menuItems" class="hidden md:block">
     <template #start>
       <div class="flex align-items-center mr-5">
-        <img alt="logo" src="/logo_origin.jpg" height="100" class="mr-2 logo-round"/>
+        <img alt="logo" src="/logo.webp" height="100" class="mr-2 logo-round"/>
         <div>
           <span class="font-bold text-xl block text-center">福州大学学生游泳协会</span>
           <span class="text-sm text-color-secondary block text-center">Fuzhou University Student Swimming Club</span>
