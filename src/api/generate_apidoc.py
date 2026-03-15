6eb451a686f61106d2b863f58d9fc2e9e589a6b7
WuJunkai2004 <wujunkai20041123@outlook.com>
2026-03-15 09:59:28 +0800

    feat: use pathlib and refactor generate_apidoc script

--- src/api/generate_apidoc.py
+++ src/api/generate_apidoc.py
@@ -1,32 +1,37 @@
-#/usr/bin python3
+# /usr/bin python3
 """
 根据json文件，生成API接口文档
 """
 
-import os
 import json
+from pathlib import Path
 
 print("开始生成 API 接口文档...")
 
 # 判断是否在主目录
-if not os.path.exists('vite.config.js'):
+if not Path("vite.config.js").is_file():
     print("请在项目主目录下运行此脚本")
     exit(1)
 
 # 判断 dist 目录是否存在, 不存在则创建
-if not os.path.exists('dist'):
-    os.makedirs('dist')
+if not Path("dist").is_dir():
+    Path("dist").mkdir()
 
-def PrevLevel(url):
+
+def PrevLevel(url: str) -> str:
     "优先级"
     prev = {
-        '/activity': '1',
-        '/leader': '2',
-        '/player': '3',
-        '/admin': '4',
-        '/sport': '5',
-        "/api": '6',
+        "/activity": "1",
+        "/leader": "2",
+        "/player": "3",
+        "/admin": "4",
+        "/sport": "5",
+        "/competition": "6",
+        "/volunteer": "7",
+        "/alumnus": "8",
     }
+    if url.startswith("/api"):
+        url = url[4:]
     for k, v in prev.items():
         url = url.replace(k, v)
     return url
@@ -38,19 +43,23 @@ def generator(api_data, api_index):
     lines.append(f"### {api_index} {api_data['description']}")
     lines.append(f"- 接口: `{api_data['url']}`")
     lines.append(f"- 方法: `{' '.join(api_data['method'])}`")
-    if api_data['params']:
-        lines.append(f"- 参数:")
-        for param, desc in api_data['params'].items():
+    if api_data["params"]:
+        lines.append("- 参数:")
+        for param, desc in api_data["params"].items():
             lines.append(f"  - `{param}`: {desc}")
     lines.append("- 返回:  ")
     lines.append("")
     lines.append("**成功**:")
     lines.append("```json")
-    lines.extend(json.dumps(api_data['response'], indent=2, ensure_ascii=False).splitlines())
+    lines.extend(
+        json.dumps(api_data["response"], indent=2, ensure_ascii=False).splitlines()
+    )
     lines.append("```")
     lines.append("**失败**:")
     lines.append("```json")
-    lines.extend(json.dumps(api_data['fail'], indent=2, ensure_ascii=False).splitlines())
+    lines.extend(
+        json.dumps(api_data["fail"], indent=2, ensure_ascii=False).splitlines()
+    )
     lines.append("```")
     lines.append("")
 
@@ -58,29 +67,36 @@ def generator(api_data, api_index):
 
 
 # json file path is the same directory as this script
-files_dir = os.path.dirname(os.path.abspath(__file__))
+files_dir = Path(__file__).parent.absolute()
 
 # glob all json files in the directory
-json_files = [f for f in os.listdir(files_dir) if f.endswith('.json')]
+json_files = [f for f in files_dir.iterdir() if f.is_file() and f.suffix == ".json"]
 data_list = []
 
 for file_name in json_files:
-    file_path = os.path.join(files_dir, file_name)
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_name, "r", encoding="utf-8") as f:
         api_data = json.load(f)
     data_list.append(api_data)
 
 # sort by PrevLevel
-data_list.sort(key=lambda x: PrevLevel(x['url']))
+data_list.sort(key=lambda x: PrevLevel(x["url"]))
+
+# 生成group
+for data in data_list:
+    url = data["url"]
+    if url.startswith("/api"):
+        url = url[4:]
+    group = url.split("/")[1]
+    data["group"] = group
 
 # generate markdown file
 markdown_content = ["# API接口文档(自动生成)"]
 
-last_group = ''
+last_group = ""
 group_id = 0
-api_id   = 0
+api_id = 0
 for api in data_list:
-    group = api['url'].split('/')[1]
+    group = api["group"]
     if group != last_group:
         group_id += 1
         api_id = 0
@@ -90,9 +106,9 @@ for api in data_list:
     markdown_content.extend(generator(api, f"{group_id}.{api_id}"))
 
 # write to file
-output_path = os.path.join('dist', './接口文档.auto.md')
-with open(output_path, 'w', encoding='utf-8') as f:
-    f.write('\n'.join(markdown_content))
+output_path = Path("dist") / "接口文档.auto.md"
+with open(output_path, "w", encoding="utf-8") as f:
+    f.write("\n".join(markdown_content))
 
 # 输出结果
 print(f"成功生成 API 接口文档: {output_path}")
